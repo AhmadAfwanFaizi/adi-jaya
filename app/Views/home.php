91dc465532663394a646e782a82f9797a6fd39f5
AhmadAfwanFaizi <ahmadafwanfaizi@gmail.com>
2022-01-04 09:52:54 +0700

    update clients

--- app/Views/home.php
+++ app/Views/home.php
@@ -210,62 +210,7 @@
         <ul id="mycarousel" class="jcarousel-skin-tango recent-jcarousel clients">
           <li>
             <a href="#">
-              <img src="assets/img/dummies/clients/client1.png" class="client-logo" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="assets/img/dummies/clients/client2.png" class="client-logo" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="assets/img/dummies/clients/client3.png" class="client-logo" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="assets/img/dummies/clients/client4.png" class="client-logo" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="assets/img/dummies/clients/client5.png" class="client-logo" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="assets/img/dummies/clients/client6.png" class="client-logo" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="assets/img/dummies/clients/client1.png" class="client-logo" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="assets/img/dummies/clients/client2.png" class="client-logo" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="assets/img/dummies/clients/client3.png" class="client-logo" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="assets/img/dummies/clients/client4.png" class="client-logo" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="assets/img/dummies/clients/client5.png" class="client-logo" alt="" />
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="assets/img/dummies/clients/client6.png" class="client-logo" alt="" />
+              <img src="<?= base_url() ?>/assets/img/clients/wing-heng.png" class="client-logo" alt="" />
             </a>
           </li>
         </ul>
